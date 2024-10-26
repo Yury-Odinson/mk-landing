32f04dda5d7817842e65b3c8b371e066bae7ff03
Yury Odintsov <yury.odinson@gmail.com>
2024-10-26 18:37:30 +0300

    feat: switch slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import {SliderItem} from "./SliderItem";
 import {sliderItems} from "../tools/store";
 import {SliderItemProps} from "../tools/types";
@@ -6,11 +6,26 @@ import ImgArrowL from "../assets/img/arrow-l.png";
 import ImgArrowR from "../assets/img/arrow-r.png";
 
 export const Slider: React.FC = () => {
+
+    const sliderRef = useRef<HTMLDivElement>(null);
+
+    const handleNext = () => {
+        if (sliderRef.current) {
+            sliderRef.current.scrollBy({left: 460, behavior: "smooth"});
+        }
+    };
+
+    const handlePrev = () => {
+        if (sliderRef.current) {
+            sliderRef.current.scrollBy({left: -460, behavior: "smooth"});
+        }
+    };
+
     return (
         <div className="slider">
             <h2 className="slider__title">Галерея карикатур</h2>
 
-            <div className="slider__items">
+            <div className="slider__items" ref={sliderRef}>
                 {sliderItems?.map((item: SliderItemProps) => (
                     <SliderItem
                         name={item.name}
@@ -23,11 +38,11 @@ export const Slider: React.FC = () => {
             </div>
 
             <div className="slider__nav">
-                <button className="slider__nav-btn">
+                <button className="slider__nav-btn" onClick={handlePrev}>
                     <img src={ImgArrowL} alt="arrow-left"/>
                 </button>
 
-                <button className="slider__nav-btn">
+                <button className="slider__nav-btn" onClick={handleNext}>
                     <img src={ImgArrowR} alt="arrow-right"/>
                 </button>
             </div>
--- src/tools/store.ts
+++ src/tools/store.ts
@@ -27,5 +27,32 @@ export const sliderItems: SliderItems = [
         description: "Ссорились, вставляя крепкие слова на языке чужом, Но это просто модно, значит, всё нормально в том.",
         imgSrc: "https://sun9-69.userapi.com/impg/2jgvyUPsdLaRYM0XSX_jGmOR5yjyHp90qiz1Gw/MWRGn6LrAi8.jpg?size=416x260&quality=95&sign=65f849f9e9df0f3c8ce865d1d51d5cb8&type=album",
         id: "4"
+    },
+    {
+        name: "2 Карикатуры МК на наши Нулевые и не только...",
+        description: "Пробовали что угодно, только не сигареты, " +
+            "Забыли мгновенно, всё было и прошло." +
+            "Но вдруг из треков, как эхо из старой плёнки," +
+            "С тоской вернусь в те дни, где узнал, что...",
+        imgSrc: "https://sun9-11.userapi.com/impg/Jv4CNir7N6COoV7RnoC7xS_F13SfeaKRIzXknw/Md_ELvt9Has.jpg?size=416x260&quality=95&sign=dc3219c282e941c985940a22f457ff45&type=album",
+        id: "5"
+    },
+    {
+        name: "2 Карикатуры МК на наши Нулевые и не только...",
+        description: "Живу вполне неплохо, сладкий чай глотаю, Печальные истории на новом MacBooke набираю.",
+        imgSrc: "https://sun9-32.userapi.com/impg/wJPCYsx_Twk5x7Q5hAUa8WcMH9XE4Ti49J7kPw/uUeSkrgAnW4.jpg?size=416x260&quality=95&sign=0329625ed4e8820a2f0cf29f75e91c22&type=album",
+        id: "6"
+    },
+    {
+        name: "2 Карикатуры МК на наши Нулевые и не только...",
+        description: "Разделять лавочки с другом — это весело, без сомненья, Но решать крупные вопросы — уже иное измерение. Как же одному справляться с задачами и там, и тут?",
+        imgSrc: "https://sun9-47.userapi.com/impg/x7Xrflr9mVBa_j6dzNdsaXVWlnC3ZHkzttITuQ/iKQbTLuAOoU.jpg?size=416x260&quality=95&sign=245c972ac673ad6918b51d7d1f920ad1&type=album",
+        id: "7"
+    },
+    {
+        name: "2 Карикатуры МК на наши Нулевые и не только...",
+        description: "Ссорились, вставляя крепкие слова на языке чужом, Но это просто модно, значит, всё нормально в том.",
+        imgSrc: "https://sun9-69.userapi.com/impg/2jgvyUPsdLaRYM0XSX_jGmOR5yjyHp90qiz1Gw/MWRGn6LrAi8.jpg?size=416x260&quality=95&sign=65f849f9e9df0f3c8ce865d1d51d5cb8&type=album",
+        id: "8"
     }
 ];
