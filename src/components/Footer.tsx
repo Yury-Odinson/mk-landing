5776c5f654bfd487473915403192503f35f1bbe6
Yury Odintsov <yury.odinson@gmail.com>
2024-10-26 18:19:37 +0300

    feat: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import HeaderLogo from "../assets/img/mk-logo 2.png";
 
 export const Footer: React.FC = () => {
     return (
         <footer>
-            <span>footer</span>
+            <div className="footer-wrapper">
+                <img src={HeaderLogo} alt="logo" />
+                <h1 className="footer__title">Концепция: АО«Редакция газеты «Московский Комсомолец» © 2024. MK.RU</h1>
+            </div>
         </footer>
     );
 };
